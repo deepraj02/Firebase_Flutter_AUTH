82c50d637faca6b640d6659b04d8afd68931d293
Deepraj Baidya <63138398+deepraj02@users.noreply.github.com>
2022-04-28 17:03:20 +0530

    I have documented the process of setting up Firebase for a Flutter application. 	Add firebase_core package to your pubspec.yaml and do  necessary changes as documented in the firebase setup page. 	In case you get any sort of error related to the api level change the following in the App Level Gradle File
    			minSdkVersion 21
    
    			targetSdkVersion 30

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MaterialApp(home: HomePage()));
+}
+class HomePage extends StatefulWidget {
+  const HomePage({ Key? key }) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text("Google-SignIn Dem"),
+      ),
+    );
+  }
+}
